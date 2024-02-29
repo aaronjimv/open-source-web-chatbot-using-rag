acf82f7600f4bd99b846f30f23cbad2c90c75411
Aaron <aaronjimv@gmail.com>
2024-02-29 12:21:27 -0800

    read html text and split it into chunks

--- src/app.py
+++ src/app.py
@@ -1,10 +1,25 @@
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 
 def get_response(user_input):
     return "I dont know"
 
+def get_vectorStrore_from_url(url):
+    # load the html text from the document and split it into chunks
+    loader = WebBaseLoader(url)
+    document = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+
+    return document_chunks
+
+
 # streamlit app config
+#
 st.set_page_config(page_title="Website Chat")
 st.title("Webiste Chat")
 if "chat_history" not in st.session_state:
@@ -21,6 +36,11 @@ if website_url is None or website_url == "":
     st.info("Please enter a website URL")
 
 else:
+    # check documents in the sidbar
+    document_chunks = get_vectorStrore_from_url(website_url)
+    with st.sidebar:
+        st.write(document_chunks)
+
     # user input
     user_query = st.chat_input("Type here...")
     if user_query is not None and user_query != "":
