be4c7776f3f1626d117999510b4043ca125156a6
Aaron <aaronjimv@gmail.com>
2024-02-29 17:36:12 -0800

    setup get_conversation_rag_chain(retriever_chain)

--- src/app.py
+++ src/app.py
@@ -5,9 +5,9 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceHubEmbeddings
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain_community.llms import Ollama
-
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 def get_response(user_input):
     return "I dont know"
@@ -47,14 +47,26 @@ def get_context_retriever_chain(vector_store):
 
     return retriver_chain
 
+def get_conversation_rag_chain(retriever_chain):
+    # create the context to generate the answer of the question
+    #
+    llm = Ollama(model="phi")
+
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+
+    stuff_document_chain = create_stuff_documents_chain(llm,prompt)
+
+    return create_retrieval_chain(retriever_chain, stuff_document_chain)
+
+
 # streamlit app config
 #
 st.set_page_config(page_title="Website Chat")
 st.title("Webiste Chat")
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [
-        AIMessage(content="Hello, I am a bot. How can I help you?"),
-    ]
 
 # sidebar setup
 with st.sidebar:
@@ -65,17 +77,33 @@ if website_url is None or website_url == "":
     st.info("Please enter a website URL")
 
 else:
-    vector_store = get_vectorStrore_from_url(website_url)
+    # sesion state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a bot. How can I help you?"),
+        ]
+    # check de vectordb
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorStrore_from_url(website_url)
+    
+    # conversation chain
+    retriver_chain = get_context_retriever_chain(st.session_state.vector_store)
 
-    retriver_chain = get_context_retriever_chain(vector_store)
+    conversation_rag_chain = get_conversation_rag_chain(retriver_chain)
 
     # user input
     user_query = st.chat_input("Type here...")
     if user_query is not None and user_query != "":
 
-        response = get_response(user_query)
-        st.session_state.chat_history.append(HumanMessage(content=user_query))
-        st.session_state.chat_history.append(AIMessage(content=response))
+        # response = get_response(user_query)
+        response = conversation_rag_chain.invoke({
+            "chat_history": st.session_state.chat_history,
+            "input": user_query
+        })
+        st.write(response)
+
+        # st.session_state.chat_history.append(HumanMessage(content=user_query))
+        # st.session_state.chat_history.append(AIMessage(content=response))
 
         retriver_documents = retriver_chain.invoke({
             "chat_history": st.session_state.chat_history,
